1c678763e42c64f7e7f2afb66e64b6cd2041b596
chongyan <chongyanx@163.com>
2023-01-11 00:42:34 +0800

    deploy

--- docs/.vitepress/algolia.ts
+++ docs/.vitepress/algolia.ts
@@ -1,7 +1,7 @@
 export default {
   appId: '1VXQHSW2GR',
   apiKey: '95418577dc6980308218c06f0f51e902',
-  indexName: 'chongyandocs',
+  indexName: 'test',
   placeholder: '请输入关键词',
   buttonText: '搜索',
 }
