8c266256c066d6e1b45929b50e7ca786f944e3b5
chongyan <chongyanx@163.com>
2023-01-11 02:58:45 +0800

    deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ import algolia from "./algolia";
 export default {
     outDir: '../dist',
     head:[
-        ['link', { rel: 'icon', href: '/svg/favicon.ico' }],
+        ['link', { rel: 'icon', href: '/favicon/favicon.svg' }],
     ],
     title: "ChongyanDocs",
     description: "BigData learning document collection",
