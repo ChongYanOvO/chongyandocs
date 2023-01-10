61e264ffe56bd945db32a8496b5dd6702accdf90
chongyan <chongyanx@163.com>
2023-01-10 23:03:38 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,15 @@
 # 忽略错误
 set -e  #有错误抛出错误
 
+rm -rf dist  #删除dist文件夹
+
 # 构建
 yarn build  #然后执行打包命令
-
-# 进入待发布的目录
-cd dist  #进到dist目录
 
 git init  #执行这些git命令
 git add -A
 git commit -m 'deploy'
 
 #提交到这个分支
-git push -f git@github.com:chongyanOvO/chongyandocs.git
+git push -f git@github.com:chongyanOvO/chongyandocs.git master
 
-cd ..
-
-rm -rf dist  #删除dist文件夹
