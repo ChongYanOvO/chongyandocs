45513fffbe4a5887b538752df3b28a56838c7e7e
chongyan <chongyanx@163.com>
2023-03-15 11:48:27 +0800

    deploy

--- docs/.vitepress/indexConfig/shellScriptConfig.ts
+++ docs/.vitepress/indexConfig/shellScriptConfig.ts
@@ -6,5 +6,10 @@ export default {
         {text: "简介", link: "/shellScript/"},
         {text: "集群分发同步脚本", link: "/knowledge/shellScript/xsync[ShellScript]/"},
         {text: "查看集群所有进程脚本", link: "/knowledge/shellScript/xcall[ShellScript]/"},
+        {text: "Hadoop集群管理脚本", link: "/knowledge/shellScript/hdp[ShellScript]/"},
+        {text: "ZooKeeper集群管理脚本", link: "/knowledge/shellScript/zk[ShellScript]/"},
+        {text: "Kafka集群管理脚本", link: "/knowledge/shellScript/kf[ShellScript]/"},
+        {text: "Hive集群管理脚本", link: "/knowledge/shellScript/hv[ShellScript]/"},
+        {text: "HBase集群管理脚本", link: "/knowledge/shellScript/hb[ShellScript]/"},
     ],
 };
